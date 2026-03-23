8478ef0d426bcde7096032edce03ef41f612464e
SagarNajardhane <sagarnajardhane@gmail.com>
2026-03-23 11:22:33 +0530

    inital commit

---
+++ src/test/java/com/example/DevOps_Ass_6/DevOpsAss6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DevOps_Ass_6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsAss6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
